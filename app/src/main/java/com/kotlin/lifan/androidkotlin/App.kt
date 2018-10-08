cf73a93626c7f6aa8d50c76b1715fa48df40a6ec
LiFan <494360059@qq.com>
2018-10-08 17:00:10 +0800

    Application单例

---
+++ app/src/main/java/com/kotlin/lifan/androidkotlin/App.kt
@@ -0,0 +1,18 @@
+package com.kotlin.lifan.androidkotlin
+
+import android.app.Application
+import kotlin.properties.Delegates
+
+/**
+ * @author by LiFan
+ * @date 2018/10/8
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+    companion object {
+        var instance : App by Delegates.notNull()
+    }
+}
