08f0873a919315220fd9e381e873d94698250aa1
LiFan <494360059@qq.com>
2019-02-14 16:14:17 +0800

    子线程加载刷新View

--- app/src/main/java/com/kotlin/lifan/androidkotlin/surface_view/VideoActivity.java
+++ app/src/main/java/com/kotlin/lifan/androidkotlin/surface_view/VideoActivity.java
@@ -2,6 +2,8 @@ package com.kotlin.lifan.androidkotlin.surface_view;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.view.SurfaceView;
 
@@ -18,6 +20,18 @@ import butterknife.OnClick;
  * Time: 下午 2:25
  */
 public class VideoActivity extends BaseActivity {
+
+    private Handler handler;
+    Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Looper.prepare();
+            handler = new Handler();
+            Looper.loop();
+        }
+    });
+
+
     @BindView(R.id.surface_layout)
     SurfaceView surfaceLayout;
     private MediaPlayer mediaPlayer;
